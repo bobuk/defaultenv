d90d266895464283df30ab330d54069fc5c0dbb6
Grigory Bakunov <thebobuk@ya.ru>
2018-04-04 18:07:08 +0300

    add description to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
-setup(name="defaultenv", version="0.0.9",
+setup(name="defaultenv", version="0.0.11",
       py_modules=['defaultenv'],
       url="http://github.com/bobuk/defaultenv",
       author="Grigory Bakunov",
       author_email='thebobuk@ya.ru',
       description='Environment and .env file reader',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
